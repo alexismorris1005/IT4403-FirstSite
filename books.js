856802ee19f5b0c7a7413e915cfadcea849589ef
alexismorris1005 <70247147+alexismorris1005@users.noreply.github.com>
2024-06-25 22:41:56 -0400

    Add files via upload

---
+++ books.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    const bookData = {
+        "kind": "books#volume",
+        "id": "Wfan6L9RGgYC",
+        "etag": "261eZV2jbUk",
+        "selfLink": "https://www.googleapis.com/books/v1/volumes/Wfan6L9RGgYC",
+        "volumeInfo": {
+            "title": "The Modern Web",
+            "subtitle": "Multi-device Web Development with HTML5, CSS3, and JavaScript",
+            "authors": ["Peter Gasston"],
+            "publisher": "No Starch Press",
+            "publishedDate": "2013",
+            "description": "A Guide to Modern Web Development. Today's web technologies are evolving at near-light speed, bringing the promise of a seamless Internet ever closer to reality. When users can browse the Web on a three-inch phone screen as easily as on a fifty-inch HDTV, what's a developer to do? Peter Gasston's The Modern Web will guide you through the latest and most important tools of device-agnostic web development, including HTML5, CSS3, and JavaScript. His plain-English explanations and practical examples emphasize the techniques, principles, and practices that you'll need to easily transcend individual browser quirks and stay relevant as these technologies are updated. Learn how to: Plan your content so that it displays fluidly across multiple devices; Design websites to interact with devices using the most up-to-date APIs, including Geolocation, Orientation, and Web Storage; Incorporate cross-platform audio and video without using troublesome plug-ins; Make images and graphics scalable on high-resolution devices with SVGUse powerful HTML5 elements to design better forms. Turn outdated websites into flexible, user-friendly ones that take full advantage of the unique capabilities of any device or browser. With the help of The Modern Web, you'll be ready to navigate the front lines of device-independent development.",
+            "categories": ["Computers / Web / Design", "Computers / Software Development & Engineering / General", "Computers / Programming Languages / JavaScript", "Computers / Programming Languages / HTML", "Computers / Web / Web Programming"],
+            "averageRating": 5,
+            "ratingsCount": 2,
+            "pageCount": 264,
+            "imageLinks": {
+                "thumbnail": "http://books.google.com/books/content?id=Wfan6L9RGgYC&printsec=frontcover&img=1&zoom=1&edge=curl&imgtk=AFLRE72OjabfS8D25KVmtZlYIRnTS_BGiV3QAoQCKvU36MikSUjmBdiXCZGFHpqiaUDEd8W6ODeenEJJOLMtFsM7-vQokoir-WwkjRxq7OMAe4mtQhJhGXgkbrLVPCDw8_4GhrgTrK73&source=gbs_api"
+            },
+            "infoLink": "https://play.google.com/store/books/details?id=Wfan6L9RGgYC&source=gbs_api"
+        }
+    };
